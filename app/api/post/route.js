6b6c6dc47c8e57c563d8d3043a8feea8ada9374f
josephdinesh024 <josephdinesh24@gmail.com>
2025-09-04 20:42:29 +0530

    first commit

---
+++ app/api/post/route.js
@@ -0,0 +1,48 @@
+import prisma from "@/lib/prisma"
+import { broadcastMessage } from "../ws/route"
+
+const res = {
+    "status" : 0,
+    "post" : null,
+    "message" : null
+}
+
+export async function GET(req, params) {
+    try {
+
+        let data = await prisma.post.findMany({
+            orderBy: {
+                updatedAt: 'desc',
+            },
+        })
+        res.status = 1
+        res.post = data
+        res.message = null
+    }catch (error) {
+        res.status = 0
+        res.post = null
+        res.message = error
+    }
+
+    return Response.json(res)
+}
+
+export async function POST(req, params) {
+    try {
+        let data = await req.json()
+        let post = await prisma.post.create({
+            data
+        })
+        broadcastMessage({"type":"new",post})
+        res.status = 1
+        res.post = post
+        res.message = null
+    }catch (error) {
+        res.status = 0
+        res.post = null
+        res.message = error
+    }
+
+    return Response.json(res)
+}
+
